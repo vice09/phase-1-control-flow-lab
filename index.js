331566232a05cc606e09ff4df180edf1233924d6
Edwin Victor Oduor Odhiambo <oduoredwin43@gmail.com>
2022-06-09 01:32:14 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const { expect } = require("chai");
+function scuberGreetingForFeet(distance){
+ if (distance <=400){
+   return 'This one is on me!'
+ } else if (distance > 2000 && distance < 2500){
+   return 'I will gladly take your thirty bucks.'
+ } else if (distance > 2500){
+   return 'No can do.'// Write your code here!
+ }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
+  city === "NYC" ? "Ok, sounds good." : "No go"
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function  switchOnCharmFromTip(tipString) {
+    if (tipString === "generous") {
+      return "Thank you so much."
+     } else if (tipString === "not as generous"){
+      return "Thank you."
+     } else if (tipString === "thanks for everything") {
+       return "Bye."
+     }
 }
